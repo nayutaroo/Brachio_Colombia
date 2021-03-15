cd73e659f9bb7ee1f0353daf640a0627a51ff274
yamanetaisei <swiftmantaisei@gmail.com>
2021-03-15 23:03:42 +0900

    wip: グループ一覧画面

--- Brachio_Colombia/Sources/View/GroupListViewController.swift
+++ Brachio_Colombia/Sources/View/GroupListViewController.swift
@@ -8,8 +8,36 @@
 import UIKit
 
 class GroupListViewController: UIViewController {
-
+    
+    @IBOutlet weak var toAddGroupButton: UIButton! {
+        didSet {
+            toAddGroupButton.setTitle("＋", for: .normal)
+        }
+    }
+    @IBOutlet weak var collectionView: UICollectionView! {
+        didSet {
+            collectionView.delegate = self
+            collectionView.dataSource = self
+            // TODO: なゆたが作ったCellをここでも使う
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+}
+
+extension GroupListViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        //TODO
+        10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        // TODO なゆたが作ったCellをここでも使う
+        let cell = UICollectionViewCell()
+        return cell
+    }
+    
+    
 }
