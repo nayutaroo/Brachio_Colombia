857223cc67f5b23bc03d456c63c242e3d82bb322
Tech-Nayuta <ketanayutawork@gmail.com>
2021-03-15 09:07:17 +0900

    Initial Commit

---
+++ Brachio_ColombiaTests/Brachio_ColombiaTests.swift
@@ -0,0 +1,33 @@
+//
+//  Brachio_ColombiaTests.swift
+//  Brachio_ColombiaTests
+//
+//  Created by 化田晃平 on R 3/03/15.
+//
+
+import XCTest
+@testable import Brachio_Colombia
+
+class Brachio_ColombiaTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
