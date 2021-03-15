d7869bcc9983572af9c7da37fc418611991feda7
Tech-Nayuta <ketanayutawork@gmail.com>
2021-03-15 10:33:51 +0900

    ファイル構造設定

--- Brachio_Colombia/Sources/SceneDelegate.swift
+++ Brachio_Colombia/Sources/SceneDelegate.swift
@@ -5,18 +5,18 @@
 //  Created by 化田晃平 on R 3/03/15.
 //
 
+
 import UIKit
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        self.window = window
+        window.rootViewController = HomeViewController()
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
