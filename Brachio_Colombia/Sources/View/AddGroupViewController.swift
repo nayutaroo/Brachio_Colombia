fcd18f83316cc53e3f9b059dc4d32d3c352eeacf
yamanetaisei <swiftmantaisei@gmail.com>
2021-03-15 22:21:58 +0900

    task追加画面のUI

--- Brachio_Colombia/Sources/View/AddGroupViewController.swift
+++ Brachio_Colombia/Sources/View/AddGroupViewController.swift
@@ -6,10 +6,36 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
-class AddGroupViewController: UIViewController {
-
+final class AddGroupViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var groupNameTextField: UITextField! {
+        didSet {
+            groupNameTextField.placeholder = "グループ名を入力"
+        }
+    }
+    @IBOutlet weak var addGroupButton: UIButton! {
+        didSet {
+            addGroupButton.setTitle("グループを追加", for: .normal)
+        }
+    }
+    
+    var disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        groupNameTextField.rx.controlEvent(.editingDidEndOnExit).asDriver()
+            .drive(onNext: { _ in
+                self.groupNameTextField.resignFirstResponder()
+            })
+            .disposed(by: disposeBag)
+        
+        addGroupButton.rx.tap.subscribe { _ in
+            // TODO: グループを追加するpostの処理
+            print("hoge")
+        }
+        .disposed(by: disposeBag)
     }
 }
