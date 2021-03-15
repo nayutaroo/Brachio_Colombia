c8101eb6f37faff13b6ab6c3da81f907af1ecb51
yamanetaisei <swiftmantaisei@gmail.com>
2021-03-16 01:05:38 +0900

    GroupListVCとAddGroupVCを作成

--- Brachio_Colombia/Sources/View/AddGroupViewController.swift
+++ Brachio_Colombia/Sources/View/AddGroupViewController.swift
@@ -6,10 +6,35 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class AddGroupViewController: UIViewController {
-
+    
+    @IBOutlet weak var groupNameTextField: UITextField! {
+        didSet {
+            groupNameTextField.placeholder = "グループ名を入力"
+        }
+    }
+    @IBOutlet weak var addGroupButton: UIButton! {
+        didSet {
+            addGroupButton.setTitle("グループを作成", for: .normal)
+        }
+    }
+    
+    var disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        addGroupButton.rx.tap.subscribe { _ in
+            // TODO: グループ追加のPOST
+        }
+        .disposed(by: disposeBag)
+        
+        groupNameTextField.rx.controlEvent(.editingDidEndOnExit).asDriver()
+            .drive(onNext: { _ in
+                self.groupNameTextField.resignFirstResponder()
+            })
+            .disposed(by: disposeBag)
     }
 }
--- Brachio_Colombia/Sources/View/GroupListViewController.swift
+++ Brachio_Colombia/Sources/View/GroupListViewController.swift
@@ -6,10 +6,46 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class GroupListViewController: UIViewController {
 
+    @IBOutlet weak var collectonView: UICollectionView!
+    @IBOutlet weak var toAddGroupButton: UIButton! {
+        didSet {
+            toAddGroupButton.setTitle("+", for: .normal)
+        }
+    }
+    private let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        toAddGroupButton.rx.tap.subscribe { _ in
+            let vc = AddGroupViewController()
+            //vc.modalPresentationStyle = .fullScreen
+            self.present(vc, animated: true)
+        }
+        .disposed(by: disposeBag)
     }
+}
+
+extension GroupListViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let vc = ProfileListViewController()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+}
+
+extension GroupListViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeue(ProfileListCell.self, for: indexPath)
+        return cell
+    }
+    
 }
