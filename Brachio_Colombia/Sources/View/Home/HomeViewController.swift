803cf0fc9224e1f73ec3ba9173b0dd2e13b190cd
Tech-Nayuta <ketanayutawork@gmail.com>
2021-03-15 10:45:30 +0900

    slackチェック

--- Brachio_Colombia/Sources/View/Home/HomeViewController.swift
+++ Brachio_Colombia/Sources/View/Home/HomeViewController.swift
@@ -11,7 +11,7 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        // print("check")
         // Do any additional setup after loading the view.
     }
 
