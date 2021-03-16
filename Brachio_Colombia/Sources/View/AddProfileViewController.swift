53675b88faa83584b03d1e8d5272efbd7b7263f8
Yoshitsugu-Antony-Kambara <tsugu1999213@yahoo.co.jp>
2021-03-16 09:12:30 +0900

    プロフィール入力画面の修正&コード定義

--- Brachio_Colombia/Sources/View/AddProfileViewController.swift
+++ Brachio_Colombia/Sources/View/AddProfileViewController.swift
@@ -9,7 +9,31 @@ import UIKit
 
 class AddProfileViewController: UIViewController {
 
+    //ライブラリから取得した画像をボタンに貼り付けるために@IBOutletで宣言
+    @IBOutlet weak var imageButton: UIButton!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var messageTextView: UITextView!
+    
+    var name: String!
+    var message: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+    }
+
+    @IBAction func addPhotoButton(_ sender: Any) {
+        //ライブラリにアクセス
+        
+        //取得したライブラリの写真をimageButtonにぶちこむ
+        
+    }
+    
+   
+
+    @IBAction func addProfile(_ sender: Any) {
+        name = nameTextField.text ?? ""
+        message = messageTextView.text ?? ""
+        //firestoreにPOSTする
     }
 }
