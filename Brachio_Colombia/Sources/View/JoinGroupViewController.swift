c9be6b749fdfe1a42f2f4051af54fbb0bd6ff08f
yamanetaisei <swiftmantaisei@gmail.com>
2021-03-17 04:16:21 +0900

    joinBtn　あど

--- Brachio_Colombia/Sources/View/JoinGroupViewController.swift
+++ Brachio_Colombia/Sources/View/JoinGroupViewController.swift
@@ -9,21 +9,16 @@ import UIKit
 
 class JoinGroupViewController: UIViewController {
 
+   
+    
+    @IBOutlet weak var groupIdTextField: UITextField!
+    @IBOutlet weak var joinButton: UIButton! {
+        didSet {
+            joinButton.setTitle("join", for: .normal)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
