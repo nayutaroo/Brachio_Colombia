bfc4d9b712971050381f020a9dab385c202e3aa5
Tech-Nayuta <ketanayutawork@gmail.com>
2021-03-19 20:00:23 +0900

    Make showRetryAlert

---
+++ Brachio_Colombia/Sources/Extension/UIViewController+.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewController+.swift
+//  Brachio_Colombia
+//
+//  Created by 化田晃平 on R 3/03/19.
+//
+
+import Foundation
+
+extension UIViewController {
+    func showRetryAlert(with error: Error, retryHandler: @escaping () -> ()) {
+        let alertController = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Close", style: .cancel, handler: nil))
+        alertController.addAction(UIAlertAction(title: "Retry", style: .default) { _ in
+            retryHandler()
+        })
+        present(alertController, animated: true)
+    }
+}
+
+
